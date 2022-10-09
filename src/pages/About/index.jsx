f61821be3726bce60604e173707d9d9e9f8143f8
bernardinorafael <rafaelferreirab2@gmail.com>
2022-10-09 02:49:24 -0300

    :zap: acessibilidade no menu dropdown

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -63,17 +63,19 @@ export function About() {
                         <Content asChild>
                             <ContentText>
                                 <p>
-                                    Após terminar o ensino médio, iniciei meus estudos em
-                                    Design de Moda, é um segmento de grande empregabilidade na
-                                    minha região de nascimento, cursei cerca de um ano de
-                                    faculdade, com a chegada da pandemia e com ela vários
-                                    questionamentos sobre a carreira profissional, decidi
-                                    analisar o cenário cautelosamente, e então entrar para a
-                                    bolha. Pesquisei muito para entender em qual área de
-                                    tecnologia eu me encaixaria. Nas horas vagas, gosto de
-                                    tomar café e passar tempo com as pessoas especiais da minha
-                                    vida, esporadicamente gosto de ler um bom livro de ficção
-                                    científica e conhecer lugares novos.{" "}
+                                    Dentre minhas experiências profissionais, destaco o
+                                    transporte, onde trabalhei por quatro anos, e digo com
+                                    propriedade, que aprendi lições valiosas sobre a operação
+                                    de uma empresa de grande porte. Exerci atividades
+                                    administrativas em todos os setores da empresa, meu foco
+                                    era aprender todo o fluxo operacional e assim ter uma visão
+                                    objetiva para exercer meu papel o mais performático
+                                    possível. Após minha chegada ao Ceará, trabalhei em uma
+                                    softhouse, onde também tive ótimas lições sobre como uma
+                                    empresa tech opera. Atualmente focando no meu aprimoramento
+                                    profissional, de onde venho criando projetos pessoais.
+                                    Sinta-se livre para me contatar profissionalmente via
+                                    E-mail ou LinkedIn.
                                 </p>
                             </ContentText>
                         </Content>
